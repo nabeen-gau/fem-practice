98ecca3da3d5138c3bc124a46f2c0c39b3202750
nabeen-gau <nabeengau@gmail.com>
2024-01-18 09:55:34 +0545

    added global stiffness generation logic

--- main.py
+++ main.py
@@ -22,23 +22,38 @@ class Frame:
     def add_members(self, *members: Member):
         self.members = members
 
+    def generate_force_vector(self):
+        self.force_vector = np.zeros(self.DOF)
+        for load in self.nodes.loads:
+            index = self.nodes.get_index(load.position)
+            for ig, il in zip(self.nodes.dof_ids[index], range(3)):
+                self.force_vector[ig] = self.nodes.loads[index].values[il]
+
     def generate_global_stiffness_matrix(self):
         self.global_k = np.zeros((self.DOF, self.DOF))
 
         for member in self.members:
             local_k = member.stiffness_matrix
-            print(local_k)
-            print(member.nodes[0].indices)
+            begin_index = self.nodes.get_index(member.begin_node)
+            end_index = self.nodes.get_index(member.end_node)
+            iterable = [*self.nodes.dof_ids[begin_index],
+                        *self.nodes.dof_ids[end_index]]
+            for ig, il in zip(iterable, range(local_k.shape[0])):
+                for jg, jl in zip(iterable, range(local_k.shape[1])):
+                    self.global_k[ig, jg] += local_k[il, jl]
+            # print(local_k)
+            # print(member.nodes[0].indices)
 
         # print(self.global_k)
 
     def solve(self):
         self.DOF = 3 * self.nodes.item_count
-        free_dofs = self.nodes.get_free_points()
-        specified_dofs = self.nodes.get_fixed_points()
+        # free_dofs = self.nodes.get_free_points()
+        # specified_dofs = self.nodes.get_fixed_points()
 
-        print(free_dofs, specified_dofs)
-        # self.generate_global_stiffness_matrix()
+        # print(free_dofs, specified_dofs)
+        self.generate_global_stiffness_matrix()
+        self.generate_force_vector()
 
     def add_supports(self, *supports: Support) -> None:
         self.supports = supports
@@ -74,4 +89,4 @@ if __name__ == "__main__":
     frame.add_supports(s1, s2, s3)
     frame.add_loads(l1, l2, l3)
     frame.solve()
-    print(m2.stiffness_matrix/8000)
+    # print(m2.stiffness_matrix/8000)
