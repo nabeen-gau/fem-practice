783d9f7267aaed16f2fd77fa1f260efc702e877f
nabeen-gau <nabeengau@gmail.com>
2024-01-19 19:22:12 +0545

    added memberload class to implement udl and uvl

--- load.py
+++ load.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 from point import Point
+from member import Member
 
 
 @dataclass
-class Load:
+class NodalLoad:
     """
     x: float
     y: float
@@ -19,8 +20,25 @@ class Load:
     def values(self) -> tuple:
         return (self.x, self.y, self.m)
 
-    def __add__(self, other: "Load"):
-        return Load(self.x+other.x, self.y+other.y, self.m+other.m)
+    def __add__(self, other: "NodalLoad"):
+        return NodalLoad(self.x+other.x, self.y+other.y, self.m+other.m)
 
-    def __sub__(self, other: "Load"):
-        return Load(self.x-other.x, self.y-other.y, self.m-other.m)
+    def __sub__(self, other: "NodalLoad"):
+        return NodalLoad(self.x-other.x, self.y-other.y, self.m-other.m)
+
+
+@dataclass
+class MemberLoad:
+    start_value: float
+    end_value: float
+    member: Member
+
+    def __add__(self, other: "MemberLoad"):
+        return MemberLoad(self.start_value+other.start_value,
+                          self.end_value+other.end_value,
+                          self.member)
+
+    def __sub__(self, other: "MemberLoad"):
+        return MemberLoad(self.start_value-other.start_value,
+                          self.end_value-other.end_value,
+                          self.member)
